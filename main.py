2f10f3974ecf2945f6dc6bb89bc3c8d37eac1cac
rk2-182 <ricardoplus@live.cl>
2022-01-06 10:30:59 -0300

    se agrega pagina no encontrada

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ app.config['MYSQL_DB'] = 'proyecto_flask'
 mysql = MySQL(app)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
 @app.route('/')
 def index():
     return render_template('index.html')
